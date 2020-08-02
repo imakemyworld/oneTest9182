301c5abf1f1e7f70460b244e7f0623bcc34b23a1
wangzw <imakemyworld@163.com>
2020-08-02 22:09:31 +0800

    注释

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
-import MenuItem from "./components/customNav/MenuItem.vue";
+import MenuItem from "./components/customNav/MenuItem.vue"; //循环调用，注册不识别
 Vue.use(ElementUI);
 Vue.component("menu-item",MenuItem);
 Vue.config.productionTip = false
